cfdc6074c734b36bf59738de5e73879c7b4e4bcf
Manish Pandey <manishpandey@rarecrew.com>
2020-04-07 02:09:40 +0530

    Added minimal files for list, add and edit modules

---
+++ NoteMe/NoteMe/Modules/Edit Note/EditNoteViewController.swift
@@ -0,0 +1,9 @@
+//
+//  EditNoteViewController.swift
+//  NoteMe
+//
+//  Created by RC-Mac-Manish on 07/04/20.
+//  Copyright © 2020 NoteMe. All rights reserved.
+//
+
+import Foundation
---
+++ NoteMe/NoteMe/Modules/Notes List/NotesListViewController.swift
@@ -0,0 +1,10 @@
+//
+//  NotesListViewController.swift
+//  NoteMe
+//
+//  Created by RC-Mac-Manish on 07/04/20.
+//  Copyright © 2020 NoteMe. All rights reserved.
+//
+
+import Foundation
+import UIKit
